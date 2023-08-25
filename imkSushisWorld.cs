8a862eff24d1d4c7c4bd440900f4d312bf42e183
imkSushi <isaackaufmann@btinternet.com>
2023-08-25 01:15:27 +0100

    Skipping time with Time Spheres now starts and stops more fluidly

--- imkSushisWorld.cs
+++ imkSushisWorld.cs
@@ -1,3 +1,4 @@
+using System;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -5,18 +6,32 @@ namespace imkSushisMod {
 public class imkSushisWorld : ModSystem
 {
     public int SkipAm = 0;
+    public int TimeSpeed = 0;
 
     public override void PreUpdateTime()
     {
-        if (SkipAm > 0)
-        {
-            Main.time += 100;
-            SkipAm--;
-        }
+        if (SkipAm <= 0) 
+            return;
+        
+        var maxTimeSpeed = MaxTimeSpeed();
+
+        if (maxTimeSpeed > TimeSpeed && TimeSpeed < 100)
+            TimeSpeed++;
+        else if (maxTimeSpeed < TimeSpeed) 
+            TimeSpeed--;
+            
+        Main.time += TimeSpeed;
+        SkipAm -= TimeSpeed;
+    }
+    
+    public int MaxTimeSpeed()
+    {
+        var eightNPlusOne = 8 * SkipAm + 1;
+        return (int)Math.Floor((Math.Sqrt(eightNPlusOne) - 1) / 2);
     }
 
     public void AddTime(int am)
     {
-    	SkipAm += am;
+    	SkipAm += am * 100;
     }
 }}
